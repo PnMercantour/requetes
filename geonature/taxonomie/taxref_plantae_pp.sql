fd6343fc5ec0255c5fc5a0f7c5d8e1cc1a5f2cc6
Vincent Prunet <vincent.prunet@gmail.com>
2022-02-01 09:17:27 +0100

    Taxref Plantae patrimoniales ou protégées

--- geonature/taxonomie/taxref_plantae_pp.sql
+++ geonature/taxonomie/taxref_plantae_pp.sql
@@ -1,9 +1,8 @@
 -- extrait du taxref pour les espèces de Flore (avec patrimonialité et protection)
 SELECT
-    patrimoniale,
-    protegee,
-    taxref.*
+  patrimoniale,
+  protegee,
+  taxref.*
 FROM
-    taxonomie.vm_plantae_pp
-    JOIN taxonomie.taxref USING (cd_nom);
-
+  taxonomie.taxref
+  JOIN taxonomie.vm_plantae_pp USING (cd_nom);
